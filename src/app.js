7db798d320d0b215ae527413c2368c7b3105e63f
botflux <victor.mendele68@gmail.com>
2019-02-11 18:16:00 +0100

    feat: user data validation middleware

--- src/app.js
+++ src/app.js
@@ -7,16 +7,31 @@ const fs = require('fs')
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 
+const userDataMiddleware = (req, res, next) => {
+    const { username, password } = req.body
+
+    if (username !== undefined && password !== undefined) {
+        req.areUserData = true
+    }
+
+    next()
+}
+
+app.use(userDataMiddleware)
+
 app.get('/', (req, res) => {
     res.send('Hello world')
 })
 
 app.post('/login', (req, res) => {
+    
     const { username, password } = req.body
-
-    console.log(username, password)
-
+    /*
     if (username === undefined || password === undefined) {
+        return res.send('You need to pass all data.')
+    }*/
+
+    if (!req.areUserData) {
         return res.send('You need to pass all data.')
     }
 
