2e741ef80b16e4470e83aa28627ad1b4a3387101
botflux <victor.mendele68@gmail.com>
2019-02-11 18:10:18 +0100

    feat: simple login

--- src/app.js
+++ src/app.js
@@ -1,12 +1,42 @@
 const express = require('express')
 const bodyParser = require('body-parser')
 const app = express()
+const jwt = require('jsonwebtoken')
+const fs = require('fs')
 
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 
 app.get('/', (req, res) => {
     res.send('Hello world')
+})
+
+app.post('/login', (req, res) => {
+    const { username, password } = req.body
+
+    console.log(username, password)
+
+    if (username === undefined || password === undefined) {
+        return res.send('You need to pass all data.')
+    }
+
+    fs.readFile('db.json', 'utf8', (e, data) => {
+        if (e) throw e
+
+        const { users } = JSON.parse(data)
+        console.log(users)
+        
+        const found = users.find(u => u.username === username && u.password === password)
+
+        if (!found) {
+            return res
+                .sendStatus(404)
+                .send('User not found.')
+        } else {
+            const token = jwt.sign(found, 'APPLICATION_SECRET_PASSWORD')
+            return res.send(token)
+        }
+    })
 })
 
 module.exports = app
